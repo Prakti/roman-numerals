c0f4e9d0591db469a69c48a69a7db0bf59eb8a33
Marcus Autenrieth <myself@prakti.org>
2017-05-21 13:34:12 +0200

    Implemented Happy Case

--- hsroman/src/Lib.hs
+++ hsroman/src/Lib.hs
@@ -3,5 +3,21 @@ module Lib
   ) where
 
 arabicToRoman :: Integer -> [Char]
-arabicToRoman arabic
-  | otherwise = [ 'I' | _ <- [1..arabic]]
+arabicToRoman arabic = arabicToRomanLoop arabic ""
+
+
+arabicToRomanLoop :: Integer -> [Char] -> [Char]
+arabicToRomanLoop arabic roman
+  | arabic > 999 = arabicToRomanLoop (arabic - 1000) (roman ++ "M")
+  | arabic > 899 = arabicToRomanLoop (arabic - 900) (roman ++ "CM")
+  | arabic > 499 = arabicToRomanLoop (arabic - 500) (roman ++ "D")
+  | arabic > 399 = arabicToRomanLoop (arabic - 400) (roman ++ "CD")
+  | arabic > 99  = arabicToRomanLoop (arabic - 100) (roman ++ "C")
+  | arabic > 89  = arabicToRomanLoop (arabic - 90)  (roman ++ "XC")
+  | arabic > 49  = arabicToRomanLoop (arabic - 50)  (roman ++ "L")
+  | arabic > 39  = arabicToRomanLoop (arabic - 40)  (roman ++ "XL")
+  | arabic > 9   = arabicToRomanLoop (arabic - 10)  (roman ++ "X")
+  | arabic > 8   = arabicToRomanLoop (arabic - 9)   (roman ++ "IX")
+  | arabic > 4   = arabicToRomanLoop (arabic - 5)   (roman ++ "V")
+  | arabic > 3   = arabicToRomanLoop (arabic - 4)   (roman ++ "IV")
+  | otherwise = roman ++ [ 'I' | _ <- [1..arabic]]
--- hsroman/test/Spec.hs
+++ hsroman/test/Spec.hs
@@ -13,3 +13,51 @@ main = hspec $ do
 
     it "Converts 4 to IV" $ do
       (arabicToRoman 4) `shouldBe` "IV"
+
+    it "Converts 5 to V" $ do
+      (arabicToRoman 5) `shouldBe` "V"
+
+    it "Converts 6 to VI" $ do
+      (arabicToRoman 6) `shouldBe` "VI"
+
+    it "Converts 9 to IX" $ do
+      (arabicToRoman 9) `shouldBe` "IX"
+
+    it "Converts 11 to XI" $ do
+      (arabicToRoman 11) `shouldBe` "XI"
+
+    it "Converts 14 to XIV" $ do
+      (arabicToRoman 14) `shouldBe` "XIV"
+
+    it "Converts 20 to XX" $ do
+      (arabicToRoman 20) `shouldBe` "XX"
+
+    it "Converts 29 to XXIX" $ do
+      (arabicToRoman 29) `shouldBe` "XXIX"
+
+    it "Converts 49 to XLIX" $ do
+      (arabicToRoman 49) `shouldBe` "XLIX"
+
+    it "Converts 54 to LIV" $ do
+      (arabicToRoman 54) `shouldBe` "LIV"
+
+    it "Converts 69 to LXIX" $ do
+      (arabicToRoman 69) `shouldBe` "LXIX"
+
+    it "Converts 91 to XCI" $ do
+      (arabicToRoman 91) `shouldBe` "XCI"
+
+    it "Converts 159 to CXLIX" $ do
+      (arabicToRoman 159) `shouldBe` "CLIX"
+
+    it "Converts 499 to CDXCIX" $ do
+      (arabicToRoman 499) `shouldBe` "CDXCIX"
+
+    it "Converts 600 to DC" $ do
+      (arabicToRoman 600) `shouldBe` "DC"
+
+    it "Converts 900 to CM" $ do
+      (arabicToRoman 900) `shouldBe` "CM"
+
+    it "Converts 2000 to MM" $ do
+      (arabicToRoman 2000) `shouldBe` "MM"
