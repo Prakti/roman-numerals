ebb22865f666da22278f88b79e1602298ae4864a
Marcus Autenrieth <myself@prakti.org>
2017-05-21 13:03:14 +0200

    Solved simple cases

--- hsroman/src/Lib.hs
+++ hsroman/src/Lib.hs
@@ -1,6 +1,7 @@
 module Lib
-    ( someFunc
-    ) where
+  ( arabicToRoman
+  ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+arabicToRoman :: Integer -> [Char]
+arabicToRoman arabic
+  | otherwise = [ 'I' | _ <- [1..arabic]]
--- hsroman/test/Spec.hs
+++ hsroman/test/Spec.hs
@@ -1,2 +1,15 @@
+import Test.Hspec
+import Control.Exception (evaluate)
+import Lib
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+  describe "arabicToRoman" $ do
+    it "Converts 1 to I" $ do
+      (arabicToRoman 1) `shouldBe` "I"
+
+    it "Converts 2 to II" $ do
+      (arabicToRoman 2) `shouldBe` "II"
+
+    it "Converts 4 to IV" $ do
+      (arabicToRoman 4) `shouldBe` "IV"
